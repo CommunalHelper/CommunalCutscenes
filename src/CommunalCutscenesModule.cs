6244472dec9e73671452fa3b8d83bf89da36e0ff
coloursofnoise <53288101+coloursofnoise@users.noreply.github.com>
2021-04-29 20:14:16 -0700

    Add project files

---
+++ src/CommunalCutscenesModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.CommunalHelper.Cutscenes {
+    public class CommunalCutscenesModule : EverestModule {
+        public override void Load() {
+        }
+
+        public override void Unload() {
+        }
+    }
+}
